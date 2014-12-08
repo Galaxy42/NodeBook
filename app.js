cc430edefdd48fbd487ac3ab1fe251c13bd646e6
Miguh Ruiz <rumomiguel@hotmail.com>
2014-12-08 14:04:56 +0100

    [Chapter 2] Serving Client-Side Assets and Adding In MiddleWare

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 var http = require("http"),
 	path = require("path"),
-	fs = require("fs");
+	fs = require("fs"),
+	extensions = {
+		".html": "text/html",
+		".css": "text/css",
+		".js": "aplication/javascript",
+		".png": "image/png",
+		".gif": "image/gif",
+		".jpg": "image/jpeg",
+	};
+var connect = require("connect");
+	connect(connect.static(_dirname + "/public")).listen(8000);
 
 http.createServer(function(req, res) {
-	function getFile (localPath, res) {
+	function getFile (localPath, mimeType, res) {
 		fs.readFile(localPath, function(err, contents) {
 			if (!err) {
-				res.end(contents);
+				res.writeHead(200, {
+					"Content-Type": mimeType,
+					"Content-length": contents.length
+				});
 			} else {
 				res.writeHead(500);
 				res.end();
@@ -15,13 +28,14 @@ http.createServer(function(req, res) {
 	}
 	var filename = path.basename(req.url) || "index.html",
 		ext = path.extname(filename),
+		dir = path.dirname(req.url).substring(1),
 		localPath = __dirname + "/public/";
 
-	if (ext == ".html") {
-		localPath += filename;
+	if (extensions[ext]) {
+		localPath += (dir ? dir + "/" : "") + filename;
 		path.exists(localPath, function(exits) {
 			if (exits) {
-				getFile(localPath, res);
+				getFile(localPath, extensions[ext], res);
 			} else {
 				res.writeHead(404);
 				res.end();
