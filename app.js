a83982d03e5c8cbf04273d5cf52e0d0f9e14b6e0
Miguh Ruiz <rumomiguel@hotmail.com>
2015-01-11 18:37:38 +0100

    [Chapter 3] Receiving Data from the Querystring

--- app.js
+++ app.js
@@ -1,46 +1,13 @@
 var http = require("http"),
-	path = require("path"),
-	fs = require("fs"),
-	extensions = {
-		".html": "text/html",
-		".css": "text/css",
-		".js": "aplication/javascript",
-		".png": "image/png",
-		".gif": "image/gif",
-		".jpg": "image/jpeg",
-	};
-var connect = require("connect");
-	connect(connect.static(_dirname + "/public")).listen(8000);
-
+	querystring = require("querystring");
+	
 http.createServer(function(req, res) {
-	function getFile (localPath, mimeType, res) {
-		fs.readFile(localPath, function(err, contents) {
-			if (!err) {
-				res.writeHead(200, {
-					"Content-Type": mimeType,
-					"Content-length": contents.length
-				});
-			} else {
-				res.writeHead(500);
-				res.end();
-			}
-		});
-	}
-	var filename = path.basename(req.url) || "index.html",
-		ext = path.extname(filename),
-		dir = path.dirname(req.url).substring(1),
-		localPath = __dirname + "/public/";
-
-	if (extensions[ext]) {
-		localPath += (dir ? dir + "/" : "") + filename;
-		path.exists(localPath, function(exits) {
-			if (exits) {
-				getFile(localPath, extensions[ext], res);
-			} else {
-				res.writeHead(404);
-				res.end();
-			}
-		});
-	}
-
+	// Parse everything after the "?" intro key/value pairs
+	var qs = querystring.parse(req.url.split("?")[1]),
+		// Property names are the same as in the querystring
+		userName = qs.firstName + " " + qs.lastName,
+		html = "<!doctype html>" +
+      "<html><head><title>Hello " + userName + "</title></head>" +
+      "<body><h1>Hello, " + userName + "!</h1></body></html>";
+      res.end(html);
 }).listen(8000);
